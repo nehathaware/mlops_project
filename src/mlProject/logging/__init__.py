b332d8479d6f5141ccb436c23f85276e1f90e936
nehathaware <neha21thaware@gmail.com>
2024-05-05 22:57:14 +0530

    logging added

--- src/mlProject/logging/__init__.py
+++ src/mlProject/logging/__init__.py
@@ -1,3 +1,24 @@
+#This functionality can be written directly in __init__.py from src instead of creating logging folder and file
+#But I am keeping it separate so as to understand it better
+
 import os
 import sys
 import logging
+
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "running_log.log")
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = logging_str,
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("mlprojectname")
